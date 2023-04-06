316b66e6e676fa042fb29957d283af63fbd1380a
Jose Andres Salazar <joseandres61@gmail.com>
2023-04-06 15:39:47 -0400

    feat: add some middlewares and dotenv conifg

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,20 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import express, { Express } from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
 import { router as userRouter } from './routes/users-routes';
-
-dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
 // Routers
 app.use('/api/users', userRouter);
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
