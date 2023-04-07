e8ee276c458809a9ded689ddc3ad9d10b3172ef1
Jose Andres Salazar <joseandres61@gmail.com>
2023-04-06 21:14:07 -0400

    fix: move middlewares place in app.ts

--- src/app.ts
+++ src/app.ts
@@ -8,13 +8,13 @@ import { router as userRouter } from './routes/users-routes';
 const app: Express = express();
 const port = process.env.PORT;
 
-// Routers
-app.use('/api/users', userRouter);
-
 // Middlewares
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
+
+// Routers
+app.use('/api/users', userRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
