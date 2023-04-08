35227b9929f7c4a6da9a345527664b9cc91701dc
Jose Andres Salazar <joseandres61@gmail.com>
2023-04-08 19:55:53 -0400

    feat: create error messages enum

---
+++ src/helpers/error-messages.ts
@@ -0,0 +1,8 @@
+export enum ErrorMessages {
+  User_not_Exists = "Sorry, this users doesn't exists ",
+  User_missing = 'You must provide an user',
+  Password_missing = 'You must provide an password',
+  Password_invalid = "Your password it's wrong",
+  Email_missing = 'Missing email',
+  Email_invalid = 'You musts provide a valid email',
+}
