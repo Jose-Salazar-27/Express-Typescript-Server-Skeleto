8ab17b6baed144f30159bcd4e3e60b252b45b6fb
Jose Andres Salazar <joseandres61@gmail.com>
2023-04-08 19:57:21 -0400

    chore: create user models

--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import { getAllUsers, searchUser, validateTest } from '../controllers/user-controllers';
+import { validateLoginUser } from '../validators/user-validator';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.json({ payload: 'all its ok' });
-});
+router.get('/', getAllUsers);
+router.get('/login', searchUser);
+router.get('/test', validateLoginUser, validateTest);
 
 export { router };
