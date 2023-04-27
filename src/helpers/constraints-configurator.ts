f9110457a2ebaabeb22facdcf3f42350056b2153
Jose Andres Salazar <joseandres61@gmail.com>
2023-04-26 20:30:25 -0400

    feat: create constraints configurator for handle roles

---
+++ src/helpers/constraints-configurator.ts
@@ -0,0 +1,55 @@
+import { AxiosRequestConfig } from 'axios';
+import { ServerConfig } from '../config/server-config';
+
+interface RoleSet {
+  [key: string]: string;
+  alpha: string;
+  legend: string;
+  tryout: string;
+}
+
+export abstract class ConstraintsConfigurator extends ServerConfig {
+  public roles: RoleSet;
+  public channels: RoleSet;
+  protected token: string;
+  protected axios_config: AxiosRequestConfig;
+
+  constructor() {
+    super();
+    this.roles = this.setRoles();
+    this.channels = this.setChannels();
+    this.token = this.getEnvVar('BOT_TOKEN');
+    this.axios_config = this.setRequestConfig();
+  }
+
+  setRoles() {
+    const roles = {
+      alpha: this.getEnvVar('ALFA_ROLE_ID'),
+      legend: this.getEnvVar('LEGEND_ROLE_ID'),
+      tryout: this.getEnvVar('TRYOUT_ROLE_ID'),
+    };
+    return roles;
+  }
+
+  setChannels() {
+    const channels = {
+      alpha: this.getEnvVar('ALFA_CHANNEL'),
+      legend: this.getEnvVar('LEGEND_CHANNEL'),
+      tryout: this.getEnvVar('TRYOUT_CHANNEL'),
+    };
+
+    return channels;
+  }
+
+  setRequestConfig() {
+    const config: AxiosRequestConfig = {
+      baseURL: 'https://discord.com/api/v9/channels',
+      headers: {
+        Authorization: `Bot ${this.token}`,
+      },
+      method: 'get',
+    };
+
+    return config;
+  }
+}
