8d0ebd1d776f93ccdf8e40fa3466cf53fee4b8df
Jose Andres Salazar <joseandres61@gmail.com>
2023-04-28 11:48:24 -0400

    chore: remove unused files

--- src/config/test-setup.ts
+++
@@ -1,8 +0,0 @@
-import supertest from 'supertest';
-import { Server } from '../server';
-
-const server = new Server();
-const app = server.getApp();
-const request = supertest(app);
-
-export { server, request };
