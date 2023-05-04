e7f88801856b7f39074e739e832a82e76b7eee52
Jose Andres Salazar <joseandres61@gmail.com>
2023-05-04 16:59:52 -0400

    more console.logs

--- src/middleware/authorizer.ts
+++ src/middleware/authorizer.ts
@@ -42,7 +42,7 @@ export class Authorizer extends ConstraintsConfigurator {
       [this.roles.legend]: 'legend',
     };
 
-    console.log(`THE ROLE NAMES COLLECTION ARE`);
+    console.log(`THE ROLE NAMES COLLECTION ARE ${roleNames}`);
 
     return roleNames[role];
   }
