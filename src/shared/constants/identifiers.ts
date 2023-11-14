8bb0a2bf96d8c7c450deca71a9a7a0542b6c755b
Jose Andres Salazar <jose.dev2708@gmail.com>
2023-11-14 15:03:20 -0400

    chore: create dependencies identifiers

---
+++ src/shared/constants/identifiers.ts
@@ -0,0 +1,10 @@
+const TYPES = {
+  UserController: Symbol.for("UserController"),
+  UserService: Symbol.for("UserService"),
+  UserRepository: Symbol.for("UserRepository"),
+  UserRotuer: Symbol.for("UserRouter"),
+  Router: Symbol.for("MainRouter"),
+  Server: Symbol.for("Server"),
+};
+
+export { TYPES };
