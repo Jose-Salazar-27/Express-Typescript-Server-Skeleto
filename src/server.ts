36f628fa27ec511aae2cb36427813201ae435e33
Jose Andres Salazar <jose.dev2708@gmail.com>
2023-11-15 11:35:02 -0400

    fix: middleware are not initialized correctly

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
-import express from "express";
-import "reflect-metadata";
-import { inject, injectable } from "inversify";
-import { MainRouter } from "./routes";
-import { ServerConfig } from "./config/server-config";
-import { loadDependencies, middlewares } from "./config/load-server-middlwares";
-import { IRouter, IServer } from "./dependency-injection";
-import { TYPES } from "./shared/constants/identifiers";
+import express from 'express';
+import 'reflect-metadata';
+import { inject, injectable } from 'inversify';
+import { MainRouter } from './routes';
+import { ServerConfig } from './config/server-config';
+import { IRouter, IServer } from './dependency-injection';
+import { TYPES } from './shared/constants/identifiers';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import bearerToken from 'express-bearer-token';
+import ErrorHandler from './middleware/error-handler';
 
 @injectable()
 export class Server extends ServerConfig implements IServer {
@@ -17,10 +20,19 @@ export class Server extends ServerConfig implements IServer {
   constructor(@inject(TYPES.Router) _router: MainRouter) {
     super();
     this.app = express();
+    // for producction
+    // this.app.use(cors({ origin: 'https://tiento-demo.vercel.app' }));
+
+    // for local dev
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(cookieParser());
+    this.app.use(bearerToken());
+    this.app.use(express.urlencoded({ extended: true }));
     this.router = _router;
-    this.port = this.getEnvVar("PORT");
+    this.port = this.getEnvVar('PORT');
     this.loadRoutes();
-    loadDependencies(this.app, middlewares);
+    this.app.use(ErrorHandler);
   }
 
   public start() {
@@ -34,7 +46,7 @@ export class Server extends ServerConfig implements IServer {
   }
 
   protected loadRoutes() {
-    this.app.use("/api", this.router.getRouter());
+    this.app.use('/api', this.router.getRouter());
   }
 
   getApp() {
