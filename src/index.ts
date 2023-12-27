53720257eb1f940f5916cafa6a932b3a776b1404
Jose Salazar <jose.dev2708@gmail.com>
2023-12-27 17:04:30 -0400

    fix env variables load

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,10 @@ import { TYPES } from './shared/constants/identifiers';
 
 const bootstrap = async (): Promise<void> => {
   return new Promise<void>((resolve, reject) => {
-    dotenv.config({ path: `${__dirname}/../.env.${process.env.NODE_ENV}` });
+    dotenv.config({
+      path:
+        process.env.NODE_ENV === 'production' ? `${__dirname}/../.env` : `${__dirname}/../.env.${process.env.NODE_ENV}`,
+    });
     resolve();
   })
     .then(() => {
