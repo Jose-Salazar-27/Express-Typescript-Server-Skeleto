6f340ede50293f8479a752eaa5735cd15b02f2c1
Jose Salazar <jose.dev2708@gmail.com>
2024-01-09 10:14:35 -0400

    test ig authorization

--- src/services/social-media-service.ts
+++ src/services/social-media-service.ts
@@ -2,7 +2,7 @@ import { inject, injectable } from 'inversify';
 import { ISocialMediaRepository } from '../dependency-injection';
 import { TYPES } from '../shared/constants';
 import { SocialMediaRepository } from '../repositories/social-media-repository';
-import { AxiosResponse, HttpStatusCode } from 'axios';
+import axios, { AxiosResponse, HttpStatusCode } from 'axios';
 import { HttpException } from '../exceptions/custom-error';
 import { DiscordMessage } from '../models/discord-messages-model';
 import { discordErrors } from '../exceptions/messages';
@@ -12,20 +12,21 @@ import { getGuildName } from '../helpers/discord-utils';
 
 @injectable()
 export class SocialMediaService {
-  private readonly repository: ISocialMediaRepository;
+  private readonly repository: SocialMediaRepository;
 
   constructor(@inject(TYPES.Social_Media.repository) readonly _repo: SocialMediaRepository) {
     this.repository = _repo;
   }
 
-  public async getInstagramPhotos(): Promise<string[]> {
+  public async getInstagramPhotos() {
     const result = await this.repository.getInstagramPhoto(readToken());
+    console.log(result);
 
     if (result.status !== HttpStatusCode.Ok) {
       throw new HttpException({ code: result.status, message: result.statusText, context: result.data });
     }
 
-    return result.data.data;
+    return result;
   }
 
   // I'm not sure if I should to adapt message date to current user local date. Can be do on future phases
@@ -45,6 +46,7 @@ export class SocialMediaService {
           content: msg.content,
           date: new Date(msg.timestamp).toLocaleDateString('en-US'),
           channel_name: getGuildName(msg.channel_id),
+          attachment: msg.attachments,
         };
       });
     } catch (error) {
