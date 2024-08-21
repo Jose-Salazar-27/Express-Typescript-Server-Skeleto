444e2d4f4c645b5ec26ffe5e782552d44d5819bd
josertx <jose.dev2708@gmail.com>
2024-08-21 12:46:29 -0400

    chore: add routes print

--- src/server.ts
+++ src/server.ts
@@ -42,6 +42,8 @@ export class Server extends ServerConfig implements IServer {
     this.server = this.app.listen(this.port, () => {
       console.log(`Server started on port ${this.port}`);
     });
+
+    this.app._router.stack.forEach(print.bind(null, []));
   }
 
   stop() {
@@ -54,5 +56,30 @@ export class Server extends ServerConfig implements IServer {
 
   getApp() {
     return this.app;
+  }
+}
+
+function print(path: any, layer: any) {
+  if (layer.route) {
+    layer.route.stack.forEach(print.bind(null, path.concat(split(layer.route.path))));
+  } else if (layer.name === 'router' && layer.handle.stack) {
+    layer.handle.stack.forEach(print.bind(null, path.concat(split(layer.regexp))));
+  } else if (layer.method) {
+    console.log('%s /%s', layer.method.toUpperCase(), path.concat(split(layer.regexp)).filter(Boolean).join('/'));
+  }
+}
+
+function split(thing: any) {
+  if (typeof thing === 'string') {
+    return thing.split('/');
+  } else if (thing.fast_slash) {
+    return '';
+  } else {
+    var match = thing
+      .toString()
+      .replace('\\/?', '')
+      .replace('(?=\\/|$)', '$')
+      .match(/^\/\^((?:\\[.*+?^${}()|[\]\\\/]|[^.*+?^${}()|[\]\\\/])*)\$\//);
+    return match ? match[1].replace(/\\(.)/g, '$1').split('/') : '<complex:' + thing.toString() + '>';
   }
 }
